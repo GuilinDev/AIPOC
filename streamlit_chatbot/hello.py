b7d46169d4dfdea0b0ce19744682f17b7043df4e
Guilin Zhang <GuilinDev@gmail.com>
2024-08-04 21:40:08 -0400

    demo

--- streamlit_chatbot/hello.py
+++ streamlit_chatbot/hello.py
@@ -2,18 +2,20 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("Hello World")
-st.write("## This is a H2 Title!")
-x = st.text_input("Movie", "Star Wars")
+st.write("Hello, world!")
+st.write("## This is a H2 Title")
 
-if st.button("Click Me"):
-    st.write(f"Your favorite movie is `{x}`")
+x = st.text_input("Movie", "Titanic")
 
+if st.button("Click me"):
+    st.write(f"your favorite Movie: {x}")
 
 data = pd.read_csv("movies.csv")
 st.write(data)
 
-
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+chart_data = pd.DataFrame(
+    pd.DataFrame(np.random.randn(20, 3),
+                 columns=["a", "b", "c"])
+)
 
 st.bar_chart(chart_data)
